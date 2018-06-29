5c275f007af7572f61d4ed45844092849d85fcd9
John Byrne <html5css3@outlook.com>
2018-06-29 17:24:11 +0100

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import { reducer as olympicsDataSaga } from '../sagas/olympicsDataSaga/reducer'
+import { reducer as mockDataSaga } from '../sagas/mockDataSaga/reducer'
 
 const reducers = combineReducers({
-  olympicsDataSaga,
+  mockDataSaga,
   form: formReducer
 })
 
